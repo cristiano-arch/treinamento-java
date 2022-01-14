2659b1df61d05692b690efbde9978c19045d2220
Cristiano Jr <cristianoandre96@gmail.com>
2022-01-13 23:39:13 -0200

    Bug: Sacar so com saldo suficiente
    Implementando Custom Exceptions

--- model/src/main/java/com/indracompany/treinamento/model/service/ContaService.java
+++ model/src/main/java/com/indracompany/treinamento/model/service/ContaService.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.indracompany.treinamento.exception.AplicacaoException;
+import com.indracompany.treinamento.exception.ExceptionValidacoes;
 import com.indracompany.treinamento.model.entity.Conta;
 import com.indracompany.treinamento.model.repository.ContaRepository;
 
@@ -20,20 +22,32 @@ public class ContaService extends GenericCrudService<Conta, Long, ContaRepositor
 	private ContaRepository contaRepository;
 	
 	public Double consultarSaldo(String agencia, String numero) {
-		
-		Conta conta = contaRepository.findByAgenciaAndNumero(agencia, numero);
-		return conta.getSaldo();
+		return encontrarConta(agencia, numero).getSaldo();
 	}
 	
 	public Conta sacar(String agencia, String numero, Double valor) {
 		
-		Conta conta = contaRepository.findByAgenciaAndNumero(agencia, numero);
-		conta.setSaldo(conta.getSaldo()-valor);
+		Conta conta = encontrarConta(agencia, numero);
 		
-		return contaRepository.save(conta);
+		if (conta.getSaldo() > valor) {
+			conta.setSaldo(conta.getSaldo()-valor);
+			return contaRepository.save(conta);
+		} else {
+			throw new AplicacaoException(ExceptionValidacoes.ERRO_SALDO_INEXISTENTE);
+		}
 	}
 	
 	public List<Conta> consultarContaCliente(String cpf) {
 		return contaRepository.findByClienteCpf(cpf);
+	}
+	
+	public Conta encontrarConta(String agencia, String numero) {
+		Conta conta = contaRepository.findByAgenciaAndNumero(agencia, numero);
+		
+		if (conta != null) {
+			return conta;
+		} else {
+			throw new AplicacaoException(ExceptionValidacoes.ERRO_CONTA_INVALIDA);
+		}
 	}
 }
